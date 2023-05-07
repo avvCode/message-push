4fffd4b021e192e964b5da3ca0691cc7574e5aeb
zyz19 <80141182@qq.com>
2023-05-07 23:54:55 +0800

    SendServiceImpl RecallServiceImpl 封装SendTaskModel 再封装成ProcessContext

--- message-service-api-impl/src/main/java/com/vv/service/api/impl/service/RecallServiceImpl.java
+++ message-service-api-impl/src/main/java/com/vv/service/api/impl/service/RecallServiceImpl.java
@@ -1,14 +1,32 @@
 package com.vv.service.api.impl.service;
 
+import com.vv.common.vo.BasicResult;
 import com.vv.service.api.domain.SendRequest;
 import com.vv.service.api.domain.SendResponse;
+import com.vv.service.api.impl.domain.SendTaskModel;
 import com.vv.service.api.service.RecallService;
+import com.vv.support.pipeline.ProcessContext;
+import com.vv.support.pipeline.ProcessController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RecallServiceImpl implements RecallService {
+    @Autowired
+    private ProcessController processController;
+
     @Override
     public SendResponse recall(SendRequest sendRequest) {
-        return null;
+        SendTaskModel sendTaskModel = SendTaskModel.builder()
+                .messageTemplateId(sendRequest.getMessageTemplateId())
+                .build();
+        ProcessContext processContext = ProcessContext.builder()
+                .code(sendRequest.getCode())
+                .needBreak(false)
+                .processModel(sendTaskModel)
+                .response(BasicResult.success())
+                .build();
+        ProcessContext process = processController.process(processContext);
+        return new SendResponse(process.getResponse().getCode(),process.getResponse().getMsg());
     }
 }
--- message-service-api-impl/src/main/java/com/vv/service/api/impl/service/SendServiceImpl.java
+++ message-service-api-impl/src/main/java/com/vv/service/api/impl/service/SendServiceImpl.java
@@ -1,20 +1,60 @@
 package com.vv.service.api.impl.service;
 
+import com.vv.common.vo.BasicResult;
 import com.vv.service.api.domain.BatchSendRequest;
 import com.vv.service.api.domain.SendRequest;
 import com.vv.service.api.domain.SendResponse;
+import com.vv.service.api.impl.domain.SendTaskModel;
 import com.vv.service.api.service.SendService;
+import com.vv.support.pipeline.ProcessContext;
+import com.vv.support.pipeline.ProcessController;
+import com.vv.support.pipeline.ProcessModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class SendServiceImpl implements SendService {
+    @Autowired
+    private ProcessController processController;
+
+
+
     @Override
     public SendResponse send(SendRequest sendRequest) {
-        return null;
+
+        SendTaskModel sendTaskModel = SendTaskModel.builder()
+                .messageTemplateId(sendRequest.getMessageTemplateId())
+                .messageParamList(Collections.singletonList(sendRequest.getMessageParam()))
+                .build();
+
+        ProcessContext context = ProcessContext.builder()
+                .code(sendRequest.getCode())
+                .processModel(sendTaskModel)
+                .needBreak(false)
+                .response(BasicResult.success()).build();
+
+        ProcessContext process = processController.process(context);
+
+        return new SendResponse(process.getResponse().getCode(), process.getResponse().getMsg());
     }
 
     @Override
     public SendResponse batchSend(BatchSendRequest batchSendRequest) {
-        return null;
+        SendTaskModel sendTaskModel = SendTaskModel.builder()
+                .messageTemplateId(batchSendRequest.getMessageTemplateId())
+                .messageParamList(batchSendRequest.getMessageParamList())
+                .build();
+
+        ProcessContext context = ProcessContext.builder()
+                .code(batchSendRequest.getCode())
+                .processModel(sendTaskModel)
+                .needBreak(false)
+                .response(BasicResult.success()).build();
+
+        ProcessContext process = processController.process(context);
+
+        return new SendResponse(process.getResponse().getCode(), process.getResponse().getMsg());
     }
 }
